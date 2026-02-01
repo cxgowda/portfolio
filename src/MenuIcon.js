09e0e3aaeae2ae7df159354c70dbd5c9c991924a
cxgowda <chirfeb@gmal.com>
2026-02-01 22:38:56 +0530

    deleted some files

--- src/MenuIcon.js
+++
@@ -1,9 +0,0 @@
-export default function MenuIcon() {
-  return (
-    <div className="menu-icon">
-      <div className="line" />
-      <div className="line" />
-      <div className="line" />
-    </div>
-  );
-}
